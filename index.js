3ee3d1797e534474c4e825bd50980a214de5188e
Iniko Matthew Jemima <109912460+InikoMatthewPro@users.noreply.github.com>
2022-09-08 13:01:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ window.onload = function() {
 
       var title = document.createElement('h1')
       title.setAttribute('id', 'title')
-      title.textContent = 'MemeChat 2.0'
+      title.textContent = 'Sus Galaxy Chat'
 
       title_inner_container.append(title)
       title_container.append(title_inner_container)
